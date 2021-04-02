64564be7c2879e4e42122322a685a4fe7502be19
NisanurBulut <nbulut@sarplojistik.com>
2021-04-02 19:29:54 +0300

    changed main color

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" />
 </head>
 <body>
-<div class="ui purple inverted borderless top fixed fluid pointing menu">
+<div class="ui violet inverted borderless top fixed fluid pointing menu">
         <a class="header item" href="{{ route('welcome') }}">AMON</a>
         <div class="right menu">
 
