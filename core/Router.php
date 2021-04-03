7a7759996332b0f80f642d079959dba8fe41eb54
NisanurBulut <nbulut@sarplojistik.com>
2021-04-03 10:29:25 +0300

    inserted comment

--- core/Router.php
+++ core/Router.php
@@ -35,6 +35,7 @@ class Router
         if (is_string($callback)) {
             return $this->renderView($callback);
         }
+        // controller burada okuyorum.
         if (is_array($callback)) {
             Application::$app->controller = new $callback[0]();
             $callback[0] = Application::$app->controller;
