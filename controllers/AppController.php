3b820fdfe88456ebaf8cad9e4053b359b06d6df5
NisanurBulut <nbulut@sarplojistik.com>
2021-04-03 10:06:18 +0300

    created appcontroller

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -2,19 +2,20 @@
 
 namespace app\controllers;
 
-use app\core\Application;
+use app\models\App;
 use app\core\Request;
 use app\core\Controller;
+use app\core\Application;
 
 class AppController extends Controller {
 
     public function __construct()
     {
-        $this->setLayout('main');
-        Application::$app->session->setFlash('success','Merhaba');
+        // Application::$app->session->setFlash('success','Merhaba');
     }
     public function index()
     {
+        // todo listed apps from db
         $params = [
             'name' => "Selam Nisanur"
         ];
@@ -23,5 +24,12 @@ class AppController extends Controller {
     public function createApp()
     {
         return $this->render('apps/forms/create-app');
+    }
+    public function storeApp(Request $request)
+    {
+
+        $appModel = new App();
+        $appModel->loadData($request->getBody());
+        return $this->render('apps/index');
     }
 }
