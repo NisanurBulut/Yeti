8c4c6618233a426bc6a2913b7fa2bb83e1e1707d
NisanurBulut <nbulut@sarplojistik.com>
2021-04-04 12:50:54 +0300

    fixed controller middleware

--- core/Controller.php
+++ core/Controller.php
@@ -3,13 +3,24 @@
 namespace app\core;
 
 use app\core\Application;
+use app\core\middlewares\BaseMiddleware;
 
 class Controller
 {
     public string $layout = 'main';
+    protected array $middlewares = [];
+    public string $action = '';
     public function __construct()
     {
 
+    }
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middlewares[] = $middleware;
+    }
+    public function getMiddlewares()
+    {
+        return $this->middlewares;
     }
     public function setLayout($layout)
     {
