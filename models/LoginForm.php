f5409822cba95dd580fff2c52e10b737148ffa46
NisanurBulut <nbulut@sarplojistik.com>
2021-04-05 21:20:37 +0300

    created addError method for without rule's attribute

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -1,6 +1,8 @@
 <?php
 
+use app\core\Application;
 use app\core\Model;
+use app\models\User;
 
 class LoginForm extends Model
 {
@@ -12,5 +14,16 @@ class LoginForm extends Model
             'email'=>[self::RULE_UNIQUE, self::RULE_EMAIL],
             'password'=>[self::RULE_REQUIRED]
         ];
+    }
+    public function login(){
+        // need to find user
+        $userEntity = new User();
+        $user=$userEntity->where(['email'=>$this->email]);
+        if(!$user)
+        {
+          $this->addError('email','Kullanıcı bulunamadı');
+        }
+
+        Application::$app->login();
     }
 }
