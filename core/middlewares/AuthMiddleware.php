c0945f73bc29705c31b51d3d4b242bef402d32fb
NisanurBulut <nbulut@sarplojistik.com>
2021-04-06 00:35:15 +0300

    defined isguest protected

--- core/middlewares/AuthMiddleware.php
+++ core/middlewares/AuthMiddleware.php
@@ -14,10 +14,10 @@ class AuthMiddleware extends BaseMiddleware
     }
     public function execute()
     {
-        // if (Application::isGuest()) {
+        if (Application::isGuest()) {
             if (empty($this->actions) || in_array(Application::$app->controller->action, $this->actions)) {
-            throw new ForbiddenException();
+                throw new ForbiddenException();
             }
-        // }
+        }
     }
 }
