9b5fea45bac70a7503a6c45bc455c5a89055cff9
NisanurBulut <nbulut@sarplojistik.com>
2021-04-06 15:04:48 +0300

    pie chart via mockup data

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 function drawChart(data) {
     Highcharts.chart('baseLine', {
         title: {
-            text: ''
+            text: 'Günlük toplam talep sayısı gösterimi'
         },
         subtitle: {
             text: 'Kaynak: Yeti talep listesi'
@@ -45,3 +45,57 @@ function drawChart(data) {
         }]
     });
 }
+
+
+Highcharts.chart('pie', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares in January, 2018'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Other',
+            y: 7.05
+        }]
+    }]
+});
