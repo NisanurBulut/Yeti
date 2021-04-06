a67d06e17b3d002109bada6d85960cf6d85feca2
NisanurBulut <nbulut@sarplojistik.com>
2021-04-06 09:57:43 +0300

    fixed raw queries with store procedures

--- core/db/Constants.php
+++ core/db/Constants.php
@@ -47,19 +47,7 @@ class Constants
         ];
     }
 
-    public const tDemandJoinWithtApp = '
-    SELECT td.id, td.title, td.description, td.state,td.status_id, ts.name AS "status", tc.name AS "color",
-    tp.app_name AS "appName", tu1.username AS "ownerUsername",tu1.image_url AS "ownerImageUrl",
-    tu2.username AS "takedUsername", tu1.name_surname AS "ownerNamesurname",tu2.image_url AS "takedImageUrl",
-    tu2.name_surname AS "takedNamesurname",
-    TIMESTAMPDIFF(HOUR, td.created_at, NOW())
-       as "differenceTime"
-    FROM tdemand td left join tapp AS tp on tp.id=td.app_id
-    left join tuser AS tu1 on tu1.id=td.owner_id
-    left join tuser AS tu2 on tu2.id=td.undertaking_id
-    left join tstatus AS ts on ts.id=td.status_id
-    left join tcolor AS tc on tc.id=ts.color_id
-    ';
+    public const tDemandJoinWithtApp = 'CALL sp_getDemandsFull';
 
-    public const tStatusJoinWithtColor = 'SELECT ts.id AS "key", ts.name AS "name", tc.name AS "color" FROM `tstatus`  As ts inner join tColor tc on tc.id=ts.color_id';
+    public const tStatusJoinWithtColor = 'CALL sp_getSituationsFull';
 }
