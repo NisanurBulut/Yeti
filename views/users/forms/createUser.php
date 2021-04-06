23a63186f3c92e6ebe5ebfee8a2f5f1ecea72308
NisanurBulut <nbulut@sarplojistik.com>
2021-04-06 19:06:38 +0300

    implemented default user image photo

--- views/users/forms/createUser.php
+++ views/users/forms/createUser.php
@@ -14,7 +14,7 @@
     </div>
     <div class="four wide field">
         <img id="icon_img" class="ui middle aligned tiny image"
-            src="https://cdn03.ciceksepeti.com/cicek/kc9124966-1/L/turuncu-araba-sari-kalanchoe-kc9124966-1-304c4c57dbe34e63a431f46f3cab98e8.jpg" />
+            src="<?php echo APP_URL . 'images/userplaceholder.jpg'; ?>" />
     </div>
 </div>
 <div class="ui segment">
