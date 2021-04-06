48d14435d4ee5b6894b51df9987d90dc597cca76
NisanurBulut <nbulut@sarplojistik.com>
2021-04-06 18:33:13 +0300

    user cant delete your self

--- views/users/components/userItem.php
+++ views/users/components/userItem.php
@@ -2,10 +2,19 @@
 
 namespace app\views\users\components;
 
+use app\core\Application;
+
 class UserItem
 {
     public static function begin($user)
     {
+        $destroyBtn = '<a class="right floated trash btnConfirmModalOpen"
+        id="%s"  href="/users/destroyUser?id=%s">
+          <i class="trash red icon"></i>
+      </a>';
+        if (Application::$app->user->id === $user->id) {
+            $destroyBtn = '';
+        }
         $finalPrint = '<div class="four wide column centerGridItems">
         <div class="ui card"><div class="content">';
         if ($user->is_admin) {
@@ -31,14 +40,18 @@ class UserItem
         <a class="left floated edit btnModalOpen" href="/users/editUser?id=%s">
             <i class="edit blue icon"></i>
         </a>
-        <a class="right floated trash btnConfirmModalOpen"
-          id="%s"  href="/users/destroyUser?id=%s">
-            <i class="trash red icon"></i>
-        </a>
+       %s
     </div>
 </div>';
-        echo sprintf($finalPrint, $user->name_surname, $user->image_url,
-        $user->username, $user->email, $user->id, $user->id, $user->id);
+        echo sprintf(
+            $finalPrint,
+            $user->name_surname,
+            $user->image_url,
+            $user->username,
+            $user->email,
+            $user->id,
+            $destroyBtn
+        );
         return new UserItem();
     }
     public static function end()
