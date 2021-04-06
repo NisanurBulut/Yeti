05f3a6abbef3b6b37c11ab7f13f274493312afa7
NisanurBulut <nbulut@sarplojistik.com>
2021-04-06 16:47:31 +0300

    refactored sp name

--- core/db/Constants.php
+++ core/db/Constants.php
@@ -51,6 +51,6 @@ class Constants
     public const spTstatusJoinWithtColor = 'CALL sp_getSituationsFull';
     public const spGetDailyDemandsForChart = 'CALL sp_getDailyDemandsForChart';
     public const spGetAppDemandCountForChart = 'CALL sp_getAppDemandCountForChart';
-    public const spGetAppDemandStateCount = 'CALL sp_getAppDemandStateCount';
+    public const spGetDemandStateCount = 'CALL sp_getDemandStateCount';
 
 }
