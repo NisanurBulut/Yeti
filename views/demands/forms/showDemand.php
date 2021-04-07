67c1b4fab849657e7c2b6a52183e704caebc7133
NisanurBulut <nbulut@sarplojistik.com>
2021-04-08 00:15:20 +0300

    semantic ui issue

--- views/demands/forms/showDemand.php
+++ views/demands/forms/showDemand.php
@@ -1,18 +1,18 @@
-<?php use app\core\db\Constants;?>
-<h4 class="ui dividing header">Talep Aşama Değişim İşlemi</h4>
-<div class="ui info ignored icon message">
-        <i class="info letter icon"></i>
-        <div class="content">
-          <div class="header"><?php $model->title ?></div>
-          <?php echo $model->description ?>
-        </div>
-      </div>
+<?php
+
+use app\core\db\Constants; ?>
+<h4 class="ui dividing header"><?php echo $model->title ?></h4>
+<div class="segment">
+  <i class="info letter blue small icon large"></i>
+  <?php echo $model->description ?>
+</div>
+<div class="ui divider"></div>
 <?php $form = app\core\form\Form::begin('/demands/changeStateDemand', 'post') ?>
 <?php echo $form->hiddenField($model, 'id') ?>
 <?php echo $form->dropdownField($model, 'state', Constants::$contants->getStates()) ?>
 </div>
 <div class="formBtnRightAlligned">
-    <button class="ui button purple" type="submit">Kaydet</button>
+  <button class="ui button purple" type="submit">Kaydet</button>
 </div>
 <?php echo app\core\form\Form::end() ?>
 <script src="<?php echo APP_URL . 'public/semanticui/semantic.js'; ?>"></script>
